e47da5ba3532c88b01d9952f01ba523ef437f85d
Yoshihiro WADA <1365196t@stu.kobe-u.ac.jp>
2016-03-11 15:55:48 +0900

    Implement MainActivity

--- app/src/main/java/xyz/dokup/appalivemonitor/MainActivity.java
+++ app/src/main/java/xyz/dokup/appalivemonitor/MainActivity.java
@@ -1,13 +1,46 @@
 package xyz.dokup.appalivemonitor;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.BindString;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.btn_monitoring)
+    Button mMonitoringButton;
+
+    @BindString(R.string.btn_disable)
+    String mDisableLabel;
+    @BindString(R.string.btn_enable)
+    String mEnableLabel;
+
+    private boolean isEnabled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    private void setButtonText() {
+        mMonitoringButton.setText(isEnabled ? mDisableLabel : mEnableLabel);
+    }
+
+    @OnClick(R.id.btn_monitoring)
+    public void onClickMonitoringButton() {
+        if(isEnabled) {
+            // サービスの終了処理
+        } else {
+            // サービスの開始処理
+        }
+
+        setButtonText();
     }
 }
